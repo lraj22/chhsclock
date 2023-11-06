5ec47431ebb92fb32e1d0d41b422f6fa7ee3a58e
Lakshya Raj <63673413+lraj22@users.noreply.github.com>
2023-11-06 14:19:05 -0800

    Try loading icons

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const fs = require("fs");
 http.createServer(function(req, res) {
 	var url = req.url.split("?")[0];
 	if(url === "/") url = "/index.html";
-	var allowlistedUrls = ["/index.html", "/main.css", "/main.js", "/README.md", "/TODO.md"];
+	var allowlistedUrls = ["/index.html", "/main.css", "/main.js", "/README.md", "/TODO.md", "/manifest.json", "/images/favicon-16.png", "/images/favicon-32.png", "/images/favicon-180.png", "/images/favicon-192.png", "/images/favicon-512.png", "/images/favicon-vector.svg"];
 	if(!allowlistedUrls.includes(url)){
 		res.writeHead(404);
 		return res.end("404 Not Found");
@@ -14,7 +14,7 @@ http.createServer(function(req, res) {
 			res.writeHead(500);
 			return res.end("500 Server Error");
 		}
-		var mime = {"html": "text/html", "css": "text/css", "js": "text/javascript", "md": "text/plain"}[url.split(".")[1]];
+		var mime = {"html": "text/html", "css": "text/css", "js": "text/javascript", "md": "text/plain", "json": "application/json", "svg": "image/svg+xml", "png": "image/png"}[url.split(".")[1]];
 		res.writeHead(200, { "Content-Type": mime });
 		res.end(data);
 	});
